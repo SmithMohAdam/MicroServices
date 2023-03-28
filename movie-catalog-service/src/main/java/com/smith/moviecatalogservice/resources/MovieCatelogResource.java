cc7630a48cc817eda69aaca6a72f99b23aa0c3f4
SmithMohAdam <smith.mo.adam@gmail.com>
2023-03-29 02:14:47 +0300

    three micro service connected to each other throught WebClient object from webflux

--- movie-catalog-service/src/main/java/com/smith/moviecatalogservice/resources/MovieCatelogResource.java
+++ movie-catalog-service/src/main/java/com/smith/moviecatalogservice/resources/MovieCatelogResource.java
@@ -5,37 +5,46 @@ import com.smith.moviecatalogservice.models.CatalogItem;
 import com.smith.moviecatalogservice.models.Movie;
 import com.smith.moviecatalogservice.models.Rating;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+
 
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatelogResource {
 
-    // @Autowired
-    // private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder wBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         CatalogItem ret =new CatalogItem() ;
         List<CatalogItem> catList =new ArrayList<>();
-        RestTemplate rest = new RestTemplate();
+        
         List<Rating> ratings = Arrays.asList(
             new Rating( "123" ,9),
             new Rating("124" ,5)
         );
 
         for(Rating rating : ratings){
-            Movie movie =rest.getForObject( "http://localhost:8082/movies/"+rating.getMovieId() , Movie.class);
+           // Movie movie =restTemplate.getForObject( "http://localhost:8082/movies/"+rating.getMovieId() , Movie.class);
+            Movie movie = wBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
             ret.setName(movie.getMovieId());
             ret.setRating(rating.getRating());
            // ret.setDesc(movie.toString());
