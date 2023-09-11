7e8daa732044c17393217cf3144fa7146a8d0fda
Mohammed <smith.mo.adam@gmail.com>
2023-09-11 21:30:57 +0100

    creating the second service movie service

--- movie-info-service/src/main/java/com/mohammed/movieinfoservice/controllers/MovieInfoController.java
+++ movie-info-service/src/main/java/com/mohammed/movieinfoservice/controllers/MovieInfoController.java
@@ -1,5 +1,17 @@
 package com.mohammed.movieinfoservice.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mohammed.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieInfoController {
     
+    @RequestMapping("/{movieId}")
+    public Movie getMovie(@PathVariable("movieId") String movieId){
+        return new Movie("transformer" , "transformer");
+    }
 }
