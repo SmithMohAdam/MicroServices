419ebccb4148c588dab4b1ff4d86cf30a51d5f7a
Mohammed <smith.mo.adam@gmail.com>
2023-09-11 15:24:49 +0100

    crating the first services movie cataloge service

---
+++ movie-cataloge-service/src/test/java/com/mohammed/moviecatalogeservice/MovieCatalogeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammed.moviecatalogeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
