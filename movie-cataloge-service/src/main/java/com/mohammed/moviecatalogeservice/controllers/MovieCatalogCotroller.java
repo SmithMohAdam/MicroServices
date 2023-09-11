3f51fc36dfd425f8d74284afb630618ff5f7cc2f
Mohammed <smith.mo.adam@gmail.com>
2023-09-11 20:37:54 +0100

    creat the first controller and model for moive catalog

--- movie-cataloge-service/src/main/java/com/mohammed/moviecatalogeservice/controllers/MovieCatalogCotroller.java
+++ movie-cataloge-service/src/main/java/com/mohammed/moviecatalogeservice/controllers/MovieCatalogCotroller.java
@@ -1,5 +1,23 @@
 package com.mohammed.moviecatalogeservice.controllers;
 
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mohammed.moviecatalogeservice.models.CatalogItem;
+
+@RestController()
+@RequestMapping("/catalog")
 public class MovieCatalogCotroller {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        return Collections.singletonList(
+            new CatalogItem("transformers" , "Action movie" , 8)
+        );
+    }
     
 }
