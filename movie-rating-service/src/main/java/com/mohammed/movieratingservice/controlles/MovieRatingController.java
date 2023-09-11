6b56a304b15b102c1744f90c271b1509ab440dbf
Mohammed <smith.mo.adam@gmail.com>
2023-09-11 22:23:37 +0100

    creating the third microservice rating service

--- movie-rating-service/src/main/java/com/mohammed/movieratingservice/controlles/MovieRatingController.java
+++ movie-rating-service/src/main/java/com/mohammed/movieratingservice/controlles/MovieRatingController.java
@@ -1,5 +1,17 @@
 package com.mohammed.movieratingservice.controlles;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mohammed.movieratingservice.models.Rating;
+
+@RestController
+@RequestMapping("/rating")
 public class MovieRatingController {
+    @RequestMapping("/{movieID}")
+    public Rating getRating(@PathVariable("movieID") String movieId){
+        return new Rating(movieId, 9);
+    }
     
 }
---
+++ movie-rating-service/src/main/java/com/mohammed/movieratingservice/models/Rating.java
@@ -0,0 +1,25 @@
+package com.mohammed.movieratingservice.models;
+
+public class Rating {
+
+    private String movie;
+    private int rate;
+    public Rating(String movie, int rate) {
+        this.movie = movie;
+        this.rate = rate;
+    }
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+    public String getMovie() {
+        return movie;
+    }
+    public int getRate() {
+        return rate;
+    }
+    
+    
+}
